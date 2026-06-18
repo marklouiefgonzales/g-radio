9f56e9cbe5920c6b1c3e24e6a154526c3bd3456f
marklouiefgonzales <mlfgonzales@yahoo.com>
2026-06-18 18:22:19 +0800

    Add service worker caching with versioning

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+var CACHE_VERSION = 'v2'; // <-- change to whatever value to force devices to download the latest version of the script (just for rare cases that their device maintained old code
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(keys.map(function(key) {
+                if (key !== CACHE_VERSION) return caches.delete(key);
+            }));
+        })
+    );
+});
+
 self.addEventListener('fetch', function(event) {});
